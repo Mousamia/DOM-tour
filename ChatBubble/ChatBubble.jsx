12514fb7bf62ba04ae77370b8441f60091b39d50
Mousamia <87888309+Mousamia@users.noreply.github.com>
2023-04-26 22:48:57 +0600

    git chat

---
+++ ChatBubble/ChatBubble.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const ChatBubble = () => {
+    return (
+        <div>
+            <div class="chat chat-start">
+  <div class="chat-image avatar">
+    <div class="w-10 rounded-full">
+      <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+    </div>
+  </div>
+  <div class="chat-bubble">It was said that you would, destroy the Sith, not join them.</div>
+</div>
+<div class="chat chat-start">
+  <div class="chat-image avatar">
+    <div class="w-10 rounded-full">
+      <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+    </div>
+  </div>
+  <div class="chat-bubble">It was you who would bring balance to the Force</div>
+</div>
+<div class="chat chat-start">
+  <div class="chat-image avatar">
+    <div class="w-10 rounded-full">
+      <img src="/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+    </div>
+  </div>
+  <div class="chat-bubble">Not leave it in Darkness</div>
+</div>
+        </div>
+    );
+};
+
+export default ChatBubble;
